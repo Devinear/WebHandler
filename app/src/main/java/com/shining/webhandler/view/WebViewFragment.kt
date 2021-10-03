057aaa32183d0204149d3f1b6294aa20425bc4e8
Devinear <leech8757@gmail.com>
2021-10-03 23:53:19 +0900

    WebView 추가

--- app/src/main/java/com/shining/webhandler/view/WebViewFragment.kt
+++ app/src/main/java/com/shining/webhandler/view/WebViewFragment.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import android.webkit.WebView
 import com.shining.webhandler.R
+import com.shining.webhandler.webview.WebChromeClientClass
+import com.shining.webhandler.webview.WebViewClientClass
 
 /**
  * WebViewFragment.kt
@@ -13,11 +17,30 @@ import com.shining.webhandler.R
 
 class WebViewFragment : BaseFragment() {
 
+    private var webView : WebView? = null
+    private val github = "https://www.naver.com/"
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_webview, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        webView = view.findViewById(R.id.web_view)
+        webView?.apply {
+            settings.javaScriptEnabled = true
+            loadUrl(github)
+            webChromeClient = WebChromeClientClass()
+            webViewClient = WebViewClientClass()
+
+            // Build.VERSION.SDK_INT >= 19
+            setLayerType(View.LAYER_TYPE_HARDWARE, null)
+        }
+//        window.setFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED, WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED)
     }
 }
