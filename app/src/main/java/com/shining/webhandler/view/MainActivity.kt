3775a7410588b9200cff6b4f55df73c9ec859f55
Devinear <leech8757@gmail.com>
2021-10-04 22:02:13 +0900

    MainActivity 시작시 Fragment 설정

--- app/src/main/java/com/shining/webhandler/view/MainActivity.kt
+++ app/src/main/java/com/shining/webhandler/view/MainActivity.kt
@@ -9,11 +9,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
 import com.shining.webhandler.R
 
-class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
-
-//    private var webView : WebView? = null
-//    private val github = "https://github.com/"
-    private val github = "https://www.naver.com/"
+class MainActivity : AppCompatActivity(R.layout.activity_main), NavigationBarView.OnItemSelectedListener {
 
     private val frWebView by lazy { WebViewFragment() }
     private val frSetting by lazy { SettingFragment() }
@@ -25,32 +21,19 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-//        webView = findViewById(R.id.webView)
-//        webView?.apply {
-//            settings.javaScriptEnabled = true
-//            loadUrl(github)
-//            webChromeClient = WebChromeClientClass()
-//            webViewClient = WebViewClientClass()
-//
-//            // Build.VERSION.SDK_INT >= 19
-//            setLayerType(View.LAYER_TYPE_HARDWARE, null)
-//        }
-//        window.setFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED, WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED)
+        initUi()
+        requestFragment()
+    }
 
+    private fun initUi() {
         findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
             .setOnItemSelectedListener(this)
     }
 
-//    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-//        // WebView BackKey
-//        if(keyCode == KeyEvent.KEYCODE_BACK && webView?.canGoBack() == true) {
-//            webView?.goBack()
-//            return true
-//        }
-//        return super.onKeyDown(keyCode, event)
-//    }
+    fun requestFragment() {
+        changeFragment(frWebView)
+    }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         val id = item.itemId
