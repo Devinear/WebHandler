fab6c22e2859be1c9f315f81fdf6f581aa8f6466
Devinear <leech8757@gmail.com>
2021-10-05 21:36:44 +0900

    Fragment 타입 구분 동작 추가

--- app/src/main/java/com/shining/webhandler/view/MainActivity.kt
+++ app/src/main/java/com/shining/webhandler/view/MainActivity.kt
@@ -4,16 +4,23 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
 import com.shining.webhandler.R
+import com.shining.webhandler.common.FragmentType
 import com.shining.webhandler.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
 
     // ViewBinding
     private lateinit var binding : ActivityMainBinding
+    private var showFragment : FragmentType
+
+    init {
+        showFragment = FragmentType.WebView
+    }
 
     companion object {
         const val TAG = "[DE][AC] Main"
@@ -33,10 +40,24 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
 
     private fun initUi() {
         binding.bottomNavigationView.setOnItemSelectedListener(this@MainActivity)
+
+        // HARDWARE ACCELERATED 필요..?
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+            WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED)
     }
 
-    fun requestFragment() {
-        changeFragment(WebViewFragment.INSTANCE)
+    private fun requestFragment(type: FragmentType = FragmentType.WebView) {
+        showFragment = type
+
+        when(type) {
+            FragmentType.WebView -> {
+                changeFragment(WebViewFragment.INSTANCE)
+            }
+            FragmentType.Setting -> {
+                changeFragment(SettingFragment.INSTANCE)
+            }
+        }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -45,10 +66,10 @@ class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListen
 
         when(id) {
             R.id.setting -> {
-                changeFragment(SettingFragment.INSTANCE)
+                requestFragment(FragmentType.Setting)
             }
             else -> {
-                changeFragment(WebViewFragment.INSTANCE)
+                requestFragment(FragmentType.WebView)
             }
         }
 
