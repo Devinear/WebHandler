c2624f62ce0cb5afc3e2f5539187835c170aecb8
Devinear <leech8757@gmail.com>
2021-10-11 21:58:06 +0900

    Permission 모듈 임시 연동

--- app/src/main/java/com/shining/webhandler/view/IntroActivity.kt
+++ app/src/main/java/com/shining/webhandler/view/IntroActivity.kt
@@ -1,9 +1,13 @@
 package com.shining.webhandler.view
 
+import android.Manifest
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.shining.npermission.NPermission
+import com.shining.npermission.PermissionListener
 import com.shining.webhandler.R
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -15,10 +19,37 @@ class IntroActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
-        GlobalScope.launch {
-            startActivity(Intent(this@IntroActivity, MainActivity::class.java))
-            finish()
+        val listener : PermissionListener = object : PermissionListener {
+
+            override fun onPermissionGranted() {
+                Toast.makeText(this@IntroActivity, "Permission Granted", Toast.LENGTH_SHORT).show()
+
+                GlobalScope.launch {
+                    startActivity(Intent(this@IntroActivity, MainActivity::class.java))
+                    finish()
+                }
+            }
+
+            override fun onPermissionDenied(deniedPermissions: List<String>) {
+                Toast.makeText(this@IntroActivity, "Permission Denied\n$deniedPermissions", Toast.LENGTH_SHORT).show()
+
+                GlobalScope.launch {
+                    startActivity(Intent(this@IntroActivity, MainActivity::class.java))
+                    finish()
+                }
+            }
         }
+
+        NPermission.create()
+            .permissionListener(listener)
+            .permissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA))
+            .rationaleTitle("Rationale Title")
+            .rationaleMessage("Rationale Message")
+            .rationaleConfirmText("Rationale Confirm")
+            .denyTitle("Deny Title")
+            .denyMessage("Deny Message")
+            .denyCloseText("Deny Close")
+            .check()
     }
 
     companion object {
