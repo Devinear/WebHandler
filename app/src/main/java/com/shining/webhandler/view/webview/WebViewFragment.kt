2b936ab24e266effd3e1b4793e5541efead5d905
Devinear <leech8757@gmail.com>
2021-11-09 23:57:13 +0900

    - WebView Input Address UI 및 동작 추가

--- app/src/main/java/com/shining/webhandler/view/webview/WebViewFragment.kt
+++ app/src/main/java/com/shining/webhandler/view/webview/WebViewFragment.kt
@@ -1,6 +1,10 @@
 package com.shining.webhandler.view.webview
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
@@ -9,10 +13,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.webkit.JavascriptInterface
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -21,6 +27,8 @@ import com.shining.nwebview.utils.WebViewSetting
 import com.shining.webhandler.databinding.LayoutWebviewBinding
 import com.shining.webhandler.util.Utils
 import com.shining.webhandler.view.base.BaseFragment
+import org.jsoup.Jsoup
+
 
 /**
  * WebViewFragment.kt
@@ -73,7 +81,6 @@ class WebViewFragment : BaseFragment(), NWebListener {
 
             textZoom = 100
             userAgentString = userAgent ?: ""
-
             loadWithOverviewMode = true                     // html의 컨텐츠가 웹뷰보다 클 경우 스크린 크기에 맞게 조정
             layoutAlgorithm = WebSettings.LayoutAlgorithm.NORMAL
 
@@ -110,6 +117,7 @@ class WebViewFragment : BaseFragment(), NWebListener {
             addJavascriptInterface(MyJavascriptInterface(), "Android")
         }
 
+        initInput()
         initResult()
     }
 
@@ -138,6 +146,66 @@ class WebViewFragment : BaseFragment(), NWebListener {
 
         }
         binding.webView.setActivityResult(activityResultLauncher)
+    }
+
+    private fun initInput() {
+        binding.apply {
+            layoutInput.visibility = View.GONE
+            ibInputBack.setOnClickListener {
+                showInputEdit(show = false)
+            }
+            ibInputSearch.setOnClickListener {
+                hideKeyboard()
+                showInputEdit(show = false)
+
+                val input = edInput.text.toString()
+                webView.loadUrl(input)
+                Toast.makeText(context, input, Toast.LENGTH_SHORT).show()
+            }
+            fabExplore.visibility = View.VISIBLE
+            fabExplore.setOnClickListener {
+                showInputEdit()
+            }
+        }
+    }
+
+    private fun showInputEdit(show: Boolean = true) {
+        binding.apply {
+            if (show) {
+                layoutInput.visibility = View.VISIBLE
+                ObjectAnimator.ofFloat(fabExplore, "alpha", 1f, 0f)
+                    .apply { start() }
+                    .addListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator?) {
+                            super.onAnimationEnd(animation)
+                            fabExplore.visibility = View.GONE
+                        }
+                    })
+                ObjectAnimator.ofFloat(layoutInput, "alpha", 0f, 1f).start()
+                ObjectAnimator.ofFloat(layoutInput, "translationY", 100f, 0f).start()
+            }
+            else {
+                fabExplore.visibility = View.VISIBLE
+                ObjectAnimator.ofFloat(fabExplore, "alpha",0f, 1f).start()
+                ObjectAnimator.ofFloat(layoutInput, "alpha",1f, 0f).start()
+                ObjectAnimator.ofFloat(layoutInput, "translationY",100f)
+                    .apply { start() }
+                    .addListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator?) {
+                            super.onAnimationEnd(animation)
+                            layoutInput.visibility = View.GONE
+                            edInput.text.clear()
+                        }
+                    })
+            }
+        }
+    }
+
+    private fun Fragment.hideKeyboard() {
+        view?.let {
+            (activity?.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager)
+                .apply { hideSoftInputFromWindow(it.windowToken, 0) }
+        }
     }
 
     override fun onResume() {
@@ -169,18 +237,11 @@ class WebViewFragment : BaseFragment(), NWebListener {
 
     fun webGoForward() = binding.webView.goForward()
 
+    override fun onPageStarted(url: String?, favicon: Bitmap?)  = Unit
 
-    override fun onPageStarted(url: String?, favicon: Bitmap?) {
-        Log.d(TAG, "onPageStarted")
-    }
+    override fun onPageFinished(url: String?)  = Unit
 
-    override fun onPageFinished(url: String?) {
-        Log.d(TAG, "onPageFinished")
-    }
-
-    override fun onPageError(errorCode: Int, description: String?, failingUrl: String?) {
-        Log.d(TAG, "onPageError")
-    }
+    override fun onPageError(errorCode: Int, description: String?, failingUrl: String?)  = Unit
 
     override fun onDownloadRequested(
         url: String?,
@@ -189,14 +250,7 @@ class WebViewFragment : BaseFragment(), NWebListener {
         contentLength: Long,
         contentDisposition: String?,
         userAgent: String?
-    ) {
-        Log.d(TAG, "onDownloadRequested")
+    )  = Unit
 
-
-
-    }
-
-    override fun onExternalPageRequest(url: String?) {
-        Log.d(TAG, "onExternalPageRequest")
-    }
+    override fun onExternalPageRequest(url: String?) = Unit
 }
